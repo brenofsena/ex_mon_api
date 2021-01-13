4635a67f0dc6308819738d5985398b6040633c96
Breno Sena <breno.fsena@gmail.com>
2021-01-12 22:32:05 -0300

    feat: adds trainer creation module

---
+++ lib/ex_mon_api/trainer/create.ex
@@ -0,0 +1,12 @@
+defmodule ExMonApi.Trainer.Create do
+  alias ExMonApi.{Repo, Trainer}
+
+  def call(params) do
+    params
+    |> Trainer.build()
+    |> create_trainer()
+  end
+
+  defp create_trainer({:ok, struct}), do: Repo.insert(struct)
+  defp create_trainer({:error, _changeset} = error), do: error
+end
